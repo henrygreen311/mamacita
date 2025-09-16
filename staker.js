177fdf75883e687ae8af232e91ffc3050876743d
henrygreen311 <105482903+henrygreen311@users.noreply.github.com>
2025-09-16 06:48:49 +0000

    Update staker.js

--- staker.js
+++ staker.js
@@ -157,7 +157,7 @@ async function runBetCycle(page, isFirstRun = false) {
   await page.goto(CHECK_URL, { waitUntil: 'domcontentloaded', timeout: 60000 });        
       
   // --- Loop for 5 min ---        
-  const runDuration = 5 * 60 * 1000; // 5 min in ms        
+  const runDuration = 55 * 60 * 1000; // 55 min in ms        
   const endTime = Date.now() + runDuration;        
   let isFirstRun = true;        
       
